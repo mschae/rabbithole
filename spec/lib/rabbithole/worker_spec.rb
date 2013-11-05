79b1350d1e89c19566da7a415f2150a15e1e357b
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2013-11-05 20:19:08 +0000

    Refactor worker spec

--- spec/lib/rabbithole/worker_spec.rb
+++ spec/lib/rabbithole/worker_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Rabbithole::Worker do
-  it 'let\'s me subscribe to queues' do
-    expect {
-      subject.listen_to_queue(Rabbithole::Connection::DEFAULT_QUEUE)
-      wait_for { Rabbithole::Connection.default_queue.consumer_count > 0 }
-    }.to change {Rabbithole::Connection.default_queue.consumer_count}.by(1)
+  before :each do
+    subject.listen_to_queue(Rabbithole::Connection::DEFAULT_QUEUE)
+  end
+
+  after :each do
     subject.stop_listening
   end
 
@@ -13,22 +13,20 @@ describe Rabbithole::Worker do
     class FooJob
       def self.perform; end
     end
+
     FooJob.should_receive(:perform).once
     Rabbithole.enqueue(FooJob)
-    subject.listen_to_queue(Rabbithole::Connection::DEFAULT_QUEUE)
     wait_for { Rabbithole::Connection.default_queue.message_count > 0 }
-    subject.stop_listening
   end
 
   it 'passes the correct arguments to the perform action' do
     class BarJob
       def self.perform(arg1, arg2); end
     end
+
     BarJob.should_receive(:perform).with(1, 'a').once
     Rabbithole.enqueue(BarJob, 1, 'a')
-    subject.listen_to_queue(Rabbithole::Connection::DEFAULT_QUEUE)
     wait_for { Rabbithole::Connection.default_queue.message_count > 0 }
-    subject.stop_listening
   end
 
   it 'gracefully handles failing jobs' do
@@ -37,8 +35,8 @@ describe Rabbithole::Worker do
         raise 'hell'
       end
     end
+
     subject.should_receive(:handle_error).twice
-    subject.listen_to_queue(Rabbithole::Connection::DEFAULT_QUEUE)
     Rabbithole.enqueue(BazJob)
     wait_for { Rabbithole::Connection.default_queue.message_count > 0 }
   end
