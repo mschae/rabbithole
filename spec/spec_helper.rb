e5df671a7075b1195ad3aa3385ea13c74bd2c612
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2013-11-05 19:22:30 +0000

    Don't require relative, use sudo only for travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,10 +6,12 @@ end
 require 'rspec'
 
 ROOT = Pathname.new(__FILE__).dirname.join('..')
-require_relative ROOT.join('lib', 'rabbithole')
+require ROOT.join('lib', 'rabbithole')
 require 'net/http'
 
 Dir[ROOT.join("spec/support/**/*.rb")].each {|f| require f}
+
+RABBITCTL_COMMAND = ENV['TRAVIS'] ? 'sudo rabbitmqctl' : 'rabbitmqctl'
 
 RSpec.configure do |config|
   config.mock_with :rspec
@@ -17,16 +19,16 @@ RSpec.configure do |config|
   config.before :suite do
     # Create test vhost
     %x{
-      sudo rabbitmqctl add_vhost #{Rabbithole::Connection::Settings.vhost}
-      sudo rabbitmqctl add_user #{Rabbithole::Connection::Settings.user} #{Rabbithole::Connection::Settings.password}
-      sudo rabbitmqctl set_permissions -p #{Rabbithole::Connection::Settings.vhost} #{Rabbithole::Connection::Settings.user} ".*" ".*" ".*"
+      #{RABBITCTL_COMMAND} add_vhost #{Rabbithole::Connection::Settings.vhost}
+      #{RABBITCTL_COMMAND} add_user #{Rabbithole::Connection::Settings.user} #{Rabbithole::Connection::Settings.password}
+      #{RABBITCTL_COMMAND} set_permissions -p #{Rabbithole::Connection::Settings.vhost} #{Rabbithole::Connection::Settings.user} ".*" ".*" ".*"
     }
   end
 
   config.after :suite do
     %x{
-      sudo rabbitmqctl delete_vhost #{Rabbithole::Connection::Settings.vhost}
-      sudo rabbitmqctl delete_user #{Rabbithole::Connection::Settings.user}
+      #{RABBITCTL_COMMAND} delete_vhost #{Rabbithole::Connection::Settings.vhost}
+      #{RABBITCTL_COMMAND} delete_user #{Rabbithole::Connection::Settings.user}
     }
   end
 end
