86c2e64d1f3d39b854cacbac3e8f5d39eba7de28
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2013-11-14 13:51:12 +0100

    Bump version

--- lib/rabbithole/version.rb
+++ lib/rabbithole/version.rb
@@ -1,6 +1,6 @@
 module Rabbithole
   VERSION_MAJOR   = 0
   VERSION_MINOR   = 0
-  VERSION_RELEASE = 2
+  VERSION_RELEASE = 3
   VERSION         = [VERSION_MAJOR, VERSION_MINOR, VERSION_RELEASE].collect(&:to_s).join('.').freeze
 end
